fca460ce0469eaec27872d6f3331e85402b96a59
Sebastian David <sebastiandavid98@example.com>
2026-04-16 15:22:47 -0500

    subiendo formulario solicitudes

--- taller_formularios_nombre_estudiantes/taller_formularios_nombre_estudiantes/urls.py
+++ taller_formularios_nombre_estudiantes/taller_formularios_nombre_estudiantes/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/asistencia/nuevo/', permanent=False), name='index'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('asistencia/', include('asistencia.urls', namespace='asistencia')),
     path('solicitudes/', include('solicitudes.urls', namespace='solicitudes')),
 ]
